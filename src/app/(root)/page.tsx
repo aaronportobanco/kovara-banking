8edabc8c41757676751f5c417df93ebf28f0e684
Aarón Portobanco <aaronportobanco@gmail.com>
2025-05-25 18:52:59 -0600

    feat: update Home component to improve user greeting and add TotalBalanceBox

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,11 @@
 import HeaderBox from "@/components/layout/HeaderBox";
+import TotalBalanceBox from "@/components/layout/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = {firstName: "Aaron", lastName: "Portobanco"};
+  // Simulating a logged-in user
+  // In a real application, you would fetch this data from an API or use a global state management solution
+  const loggedIn = { firstName: "Aaron", lastName: "Portobanco" };
   const user = loggedIn.firstName + " " + loggedIn.lastName;
 
   return (
@@ -10,12 +13,13 @@ const Home = () => {
       <div className="home-content">
         <header className="home-header">
           <HeaderBox
-            type="title"
-            title={`Welcome, ${loggedIn.firstName}`}
-            subtext="Your account balance is $10,000"
+            type="greeting" // type can be "title" or "greeting"
+            title="Welcome back,"
+            subtext="Acces and manage your bank account easily and securely."
             user={user ? user : "Guest"}
           />
         </header>
+        <TotalBalanceBox accounts={[]} totalBanks={2} totalCurrentBalance={1243.45} />
       </div>
     </section>
   );
