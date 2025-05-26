2474c7a6f415d9f451eee1f86f2f6df472fe8e41
Aarón Portobanco <aaronportobanco@gmail.com>
2025-05-25 23:41:29 -0600

    feat: refactor RootLayout to integrate AppSidebar and SiteHeader with SidebarProvider for enhanced layout management

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,12 +1,26 @@
-import Sidebar from "@/components/layout/Sidebar";
+import AppSidebar from "@/components/layout/sidebar/AppSidebar";
+import SiteHeader from "@/components/layout/sidebar/SiteHeader";
+import {
+  SidebarProvider,
+  SidebarInset,
+} from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
   return (
-    <main className="flex h-screen w-full">
-      <Sidebar /> 
-      {children}
-    </main>
+    <SidebarProvider defaultOpen={defaultOpen} >
+      <AppSidebar />
+      <SidebarInset className="rounded-[10px] shadow-md m-3">
+        <SiteHeader />
+        <main className="flex h-screen px-4">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
