0f7365e69d9d7ed87665db9b6a6265560de1d5bc
Aarón Portobanco <aaronportobanco@gmail.com>
2025-05-25 19:27:40 -0600

    feat: add Logo component for branding in layout

---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const Logo = ({ className = "" }) => {
+  return (
+    <div className={`flex items-center gap-3 z-10 ${className}`}>
+      <Image
+        src=" /icons/favicon-dark.ico"
+        alt="Logo de Kovara"
+        width={18}
+        height={27}
+      />
+      <span className="text-foreground text-base font-bold leading-7 tracking-[-0.08px]">
+        Kovara
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
