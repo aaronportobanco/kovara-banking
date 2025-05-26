bc2f53bc00073d2d49b50ed1c7383addc8b57b94
Aarón Portobanco <aaronportobanco@gmail.com>
2025-05-25 18:36:04 -0600

    fix: correct TotalBalanceBoxProps interface name and improve chart integration

--- src/components/layout/TotalBalanceBox.tsx
+++ src/components/layout/TotalBalanceBox.tsx
@@ -1,24 +1,24 @@
+import ChartPie from "../layout/charts/PieChart";
 import AnimatedCounter from "../animation/AnimatedCounter";
 import React from "react";
+import { accounts as accountsData } from "../../../constants"; // importa la data
 
 const TotalBalanceBox = ({
-  //accounts = [],
   totalBanks,
   totalCurrentBalance,
-}: TotlaBalanceBoxProps) => {
+}: TotalBalanceBoxProps) => {
   return (
     <section className="total-balance">
       <div className="total-balance-chart">
-        {/* DoughnutChart */}
-        {/* Chart or graphical representation of the total balance can go here */}
+        <ChartPie accounts={accountsData} /> {/* usa la data importada */}
       </div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2"> Bank Accounts: {totalBanks}</h2>
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
-          <p className="total-balance-amount flex-center gap-2">
+          <div className="total-balance-amount flex-center gap-2">
             <AnimatedCounter amount={totalCurrentBalance} />
-          </p>
+          </div>
         </div>
       </div>
     </section>
