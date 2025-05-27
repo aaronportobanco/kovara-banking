37192e19e45c8d3729ee74e259f9731000322ea1
Aarón Portobanco <aaronportobanco@gmail.com>
2025-05-27 00:33:56 -0600

    fix: update SiteHeader font weight for improved text consistency

--- src/components/layout/sidebar/SiteHeader.tsx
+++ src/components/layout/sidebar/SiteHeader.tsx
@@ -10,7 +10,7 @@ function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+        <h1 className="text-base font-normal">Documents</h1>
       </div>
     </header>
   )
