b4eabb80aa5752b218774f0833a37cd9c5d737b7
Aarón Portobanco <aaronportobanco@gmail.com>
2025-05-27 21:46:58 -0600

    feat: add RightSidebar component for layout structure

---
+++ src/components/layout/right-sidebar/RightSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
+  return (
+    <aside className="right-sidebar">RightSidebar</aside>
+  )
+}
+
+export default RightSidebar
