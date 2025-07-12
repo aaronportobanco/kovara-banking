7d422d96d5c2a0dcd9d5e94ce52e9b0cd331f19c
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-12 17:14:26 -0600

    refactor: remove unused layout components and charts for cleaner codebase

--- src/components/layout/right-sidebar/RightSidebar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
-  return (
-    <aside className="right-sidebar">RightSidebar</aside>
-  )
-}
-
-export default RightSidebar
