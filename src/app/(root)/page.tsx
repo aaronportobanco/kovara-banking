9caceec3defe254c733d22ef6e07e23cdcf0ec6c
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-12 17:16:18 -0600

    feat: update Home component to include email in logged-in user and add RightSidebar

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,16 @@
-import HeaderBox from "@/components/layout/HeaderBox";
-import TotalBalanceBox from "@/components/layout/TotalBalanceBox";
+import HeaderBox from "./components/HeaderBox";
+import TotalBalanceBox from "./components/TotalBalanceBox";
+import RightSidebar from "./components/rightSidebar/RightSidebar";
 import React from "react";
 
 const Home = () => {
   // Simulating a logged-in user
   // In a real application, you would fetch this data from an API or use a global state management solution
-  const loggedIn = { firstName: "Aaron", lastName: "Portobanco" };
+  const loggedIn = {
+    firstName: "Aaron",
+    lastName: "Portobanco",
+    email: "aaron@example.com",
+  };
   const user = loggedIn.firstName + " " + loggedIn.lastName;
 
   return (
@@ -19,8 +24,19 @@ const Home = () => {
             user={user ? user : "Guest"}
           />
         </header>
-        <TotalBalanceBox accounts={[]} totalBanks={2} totalCurrentBalance={1243.45} />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={2}
+          totalCurrentBalance={1243.45}
+        />
       </div>
+      <RightSidebar
+      //@ts-expect-error // Assuming RightSidebar expects a user object
+        user={loggedIn}
+        transactions={[]}
+      //@ts-expect-error // Assuming RightSidebar expects a banks array
+        banks={[{ currentBalance: 1000 }, { currentBalance: 2000 }]}
+      />
     </section>
   );
 };
