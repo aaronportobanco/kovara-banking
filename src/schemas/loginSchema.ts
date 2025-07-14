136d30f18bd96d975e8285505cfd839b39f42536
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-13 22:29:18 -0600

    refactor: remove username validation from login schema

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -1,22 +1,21 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  username: z
-    .string()
-    .min(3, { message: "Username must be at least 3 characters long" })
-    .max(20, { message: "Username must be at most 20 characters long" })
-    .regex(/^[a-zA-Z0-9_]+$/, {
-      message: "Username can only contain letters, numbers, and underscores",
-    }),
-
+  email: z.email({
+    error: (issue) =>
+      issue.input === undefined || issue.input === null
+        ? "Email is required"
+        : "Invalid email format",
+  }),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters long" })
-    .max(50, { message: "Password must be at most 50 characters long" })
-    .regex(/[A-Za-z]/, { message: "Password must contain at least one letter" })
-    .regex(/\d/, { message: "Password must contain at least one number" })
+    .nonempty({ error: "Password is required" })
+    .min(8, { error: "Password must be at least 8 characters long" })
+    .max(50, { error: "Password must be at most 50 characters long" })
+    .regex(/[A-Za-z]/, { error: "Password must contain at least one letter" })
+    .regex(/\d/, { error: "Password must contain at least one number" })
     .regex(/^[A-Za-z0-9]*$/, {
-      message: "Password can only contain letters and numbers",
+      error: "Password can only contain letters and numbers",
     }),
 });
 
