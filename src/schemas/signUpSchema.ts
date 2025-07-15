f977f2fa81c987a304ad73cbcce62375ba4ffefe
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-15 17:07:44 -0600

    feat: add SignUpSchema for user registration validation

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,68 @@
+import { z } from "zod";
+import { MAX_AGE } from "../../constants";
+
+export const SignUpSchema = z.object({
+  firstname: z
+    .string({ error: "First name must be a string" })
+    .nonempty({ error: "First name is required" })
+    .min(3, { error: "First name must be at least 3 characters long" })
+    .max(50, { error: "First name must be at most 50 characters long" }),
+  lastname: z
+    .string({ error: "Last name must be a string" })
+    .nonempty({ error: "Last name is required" })
+    .min(3, { error: "Last name must be at least 3 characters long" })
+    .max(50, { error: "Last name must be at most 50 characters long" }),
+  address: z
+    .string()
+    .nonempty({ error: "Address is required" })
+    .min(10, { error: "Address must be at least 10 characters long" })
+    .max(100, { error: "Address must be at most 100 characters long" }),
+  dateofbirth: z
+    .string()
+    .nonempty({ error: "Date of birth is required" })
+    .min(10, { error: "Date of birth must be at least 10 characters long" })
+    .max(10, { error: "Date of birth must be at most 10 characters long" })
+    .refine(
+      (str) => /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/.test(str),
+      {
+        error: "Formato debe ser YYYY-MM-DD",
+      }
+    )
+    .refine(
+      (str) => {
+        const date = new Date(str);
+        return !isNaN(date.getTime());
+      },
+      {
+        error: "Fecha inválida",
+      }
+    )
+    .refine(
+      (str) => {
+        const birthDate = new Date(str);
+        const today = new Date();
+        return birthDate < today;
+      },
+      {
+        error: "La fecha de nacimiento no puede ser en el futuro",
+      }
+    )
+    .refine(
+      (str) => {
+        const birthDate = new Date(str);
+        const today = new Date();
+        const hundredYearsAgo = new Date(
+          today.getFullYear() - MAX_AGE,
+          today.getMonth(),
+          today.getDate()
+        );
+        return birthDate > hundredYearsAgo;
+      },
+      {
+        error: `La edad no puede ser mayor a ${MAX_AGE} años`,
+      }
+    ),
+});
+
+// Export the type for use in the form
+export type SignUpSchemaType = z.infer<typeof SignUpSchema>;
