551dc80b54fb890ef23e87c1df406be0beb7a5d9
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-25 14:24:24 -0600

    feat: implement Appwrite client and user session management functions

---
+++ src/lib/server/appwrite.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { Client, Account } from "node-appwrite";
+import { cookies } from "next/headers";
+
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+  const session = (await cookies()).get("my-custom-session");
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+/*
+ * Build a utility function to get the logged in user from Appwrite.
+ * This function will be used in our components and routes
+ * to check if a user is logged in, and access the user's details.
+ */
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return null;
+  }
+}
