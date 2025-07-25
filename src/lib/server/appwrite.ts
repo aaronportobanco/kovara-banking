baf0faa49099a87b8f069a6f3207abc42df42913
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-25 16:08:39 -0600

    feat: update user actions to import getLoggedInUser function for user session management

--- src/lib/server/appwrite.ts
+++ src/lib/server/appwrite.ts
@@ -1,32 +1,27 @@
 "use server";
 
-import { Client, Account } from "node-appwrite";
+import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
+/*
+ * This function is responsible for creating a new Appwrite client instance
+ * and returning an object with methods to interact with the Appwrite account service.
+ * this gonna valide the session cookie and return the account object if the session is valid.
+ * If the session is invalid, it will throw an error.
+ */
+
 export async function createSessionClient() {
+  // Create a new Appwrite client instance with the endpoint and project ID
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  const session = (await cookies()).get("my-custom-session");
+  const session = (await cookies()).get("appwrite-session");
   if (!session || !session.value) {
     throw new Error("No session");
   }
 
-  client.setSession(session.value);
-
-  return {
-    get account() {
-      return new Account(client);
-    },
-  };
-}
-
-export async function createAdminClient() {
-  const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-    .setKey(process.env.NEXT_APPWRITE_KEY!);
+  client.setSession(session.value); // Attach the session cookie to the client
 
   return {
     get account() {
@@ -36,16 +31,30 @@ export async function createAdminClient() {
 }
 
 /*
- * Build a utility function to get the logged in user from Appwrite.
- * This function will be used in our components and routes
- * to check if a user is logged in, and access the user's details.
+ * Create a new Appwrite client instance with admin privileges
+ * This client will be used to manage users, databases, and other resources.
+ * It uses the API key for admin access, which provides full control over the Appwrite project.
+ * This is useful for administrative tasks such as user management, database operations, etc.
  */
-export async function getLoggedInUser() {
-  try {
-    const { account } = await createSessionClient();
-    return await account.get();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    return null;
-  }
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    // Set the API key for admin access, providing all the functionality
+    // to manage users, projects, and other resources.
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+
+    get database() {
+      return new Databases(client);
+    },
+    get users() {
+      return new Users(client);
+    },
+  };
 }
