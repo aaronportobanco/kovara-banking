b4e08a762c41f843cf51e7761f76f1948c51f498
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-25 21:30:02 -0600

    feat: add Toaster component to RootLayout for enhanced UI notifications

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,12 @@
+import { Toaster } from "@/components/ui/sonner";
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      {children}
+      <Toaster richColors/>
+    </main>
+  );
 }
