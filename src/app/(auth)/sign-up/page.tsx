ed60c69191fc40e0fa7087899001d25f47392543
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-27 16:50:32 -0600

    feat: implement user redirection in SignUpPage based on authentication status

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import SignUpForm from "./SignUpForm";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-
-const SignUpPage = () => {
+const SignUpPage = async () => {
+  const user = await getLoggedInUser();
+  if(user) redirect ("/plaid-link")
+  
   return (
     <section className="flex-center size-full max-sm:px-6">
       <SignUpForm />
