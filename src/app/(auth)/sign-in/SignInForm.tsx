c2cc6ed1cb810acda47c29a19770a2a08c5ba65e
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-27 21:11:43 -0600

    feat: implement sign-in logic with error handling and loading state

--- src/app/(auth)/sign-in/SignInForm.tsx
+++ src/app/(auth)/sign-in/SignInForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, LoginSchemaType } from "@/schemas/loginSchema";
@@ -7,7 +8,9 @@ import { useForm } from "react-hook-form";
 import FormFieldInput from "../components/FormFieldInput";
 import { Button } from "@/components/ui/button";
 import { Github, Loader2 } from "lucide-react";
+import { signIn } from "@/lib/actions/user.actions";
 import Link from "next/link";
+import { toast } from "sonner";
 
 const SignInForm = () => {
   const [isLoading, setIsLoading] = React.useState(false);
@@ -28,10 +31,25 @@ const SignInForm = () => {
 
   // This function will be called when the form is submitted and the data is valid
   const onSubmit = async (data: LoginSchemaType) => {
-    // Handle form submission logic here
     setIsLoading(true);
+
+    try {
+      await signIn(data);
+      console.log("Form submitted successfully", data);
+      toast.success("Sign In successful! Welcome back!", {
+        position: "top-center",
+      });
+    } catch (error) {
+      console.error("Error signing in:", error);
+      toast.error("Sign In failed. Please try again later.", {
+        position: "top-center",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+
+    // Handle form submission logic here
     // Simula una llamada a API (ej. 3 segundos)
-    await new Promise((res) => setTimeout(res, 3000));
     console.log("Form submitted with data:", data);
     setIsLoading(false);
     form.reset();
@@ -47,60 +65,62 @@ const SignInForm = () => {
           </p>
         </div>
       </header>
-        <div>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="flex flex-col gap-6"
+      <div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-6"
+          >
+            <FormFieldInput
+              control={form.control}
+              name="email"
+              label="Email"
+              type="email"
+              autoComplete="on"
+              placeholder="Ingresa tu correo electrónico"
+              description="example: user@gmail.com"
+            />
+            <FormFieldInput
+              control={form.control}
+              name="password"
+              label="Password"
+              type="password"
+              autoComplete="off"
+              minLength={8}
+              maxLength={50}
+              placeholder="Ingresa tu contraseña"
+              description="Al menos 8 caracteres y debe incluir letras y numeros"
+            />
+            <Button
+              type="submit"
+              disabled={isSubmitting || !isValid || isLoading}
+              className="form-btn"
             >
-              <FormFieldInput
-                control={form.control}
-                name="email"
-                label="Email"
-                type="email"
-                autoComplete="on"
-                placeholder="Ingresa tu correo electrónico"
-                description="example: user@gmail.com"
-              />
-              <FormFieldInput
-                control={form.control}
-                name="password"
-                label="Password"
-                type="password"
-                autoComplete="off"
-                minLength={8}
-                maxLength={50}
-                placeholder="Ingresa tu contraseña"
-                description="Al menos 8 caracteres y debe incluir letras y numeros"
-              />
-              <Button
-                type="submit"
-                disabled={isSubmitting || !isValid || isLoading}
-                className="form-btn"
+              {isLoading ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : null}
+              Log In
+            </Button>
+            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
+            <Button type="button" className="w-full" variant="outline">
+              <Github /> Login with GitHub
+            </Button>
+            <footer className="text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link
+                href="/sign-up"
+                className="underline underline-offset-4 text-bankGradient"
               >
-                {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
-                Log In
-              </Button>
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-              </div>
-              <Button type="button" className="w-full" variant="outline">
-                <Github /> Login with GitHub
-              </Button>
-              <footer className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <Link
-                  href="/sign-up"
-                  className="underline underline-offset-4 text-bankGradient"
-                >
-                  Sign up
-                </Link>
-              </footer>
-            </form>
-          </Form>
-        </div>
+                Sign up
+              </Link>
+            </footer>
+          </form>
+        </Form>
+      </div>
     </section>
   );
 };
