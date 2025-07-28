a3a6722e22268609938ccfd3175db9cdc63f7e50
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-27 19:02:10 -0600

    fix: comment out redirect logic in SignUpPage for clarity and future implementation

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import SignUpForm from "./SignUpForm";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
-import { redirect } from "next/navigation";
+//import { redirect } from "next/navigation";
 
 const SignUpPage = async () => {
   const user = await getLoggedInUser();
-  if(user) redirect ("/plaid-link")
+  // If the user is already logged in, redirect to the Plaid link page
+  //if (user) redirect("/plaid-link");
+    console.log("User in SignUpPage:", user);
+
+  // If the user is not logged in, render the sign-up form
+  // This ensures that the sign-up page is only accessible to unauthenticated users
+  // and prevents authenticated users from accessing it.
   
   return (
     <section className="flex-center size-full max-sm:px-6">
