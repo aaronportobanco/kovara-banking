fb4a1503f56212b24643bb120db7fbbe11923a85
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-27 23:50:32 -0600

    refactor: comment out user authentication logic in sign-up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import SignUpForm from "./SignUpForm";
-import { getLoggedInUser } from "@/lib/actions/user.actions";
-//import { redirect } from "next/navigation";
+/* import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation"; */
 
 const SignUpPage = async () => {
-  const user = await getLoggedInUser();
+  //const user = await getLoggedInUser();
   // If the user is already logged in, redirect to the Plaid link page
   //if (user) redirect("/plaid-link");
-    console.log("User in SignUpPage:", user);
 
   // If the user is not logged in, render the sign-up form
   // This ensures that the sign-up page is only accessible to unauthenticated users
