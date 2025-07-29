26b36813e2873e44c0f4e11a774f4f9eb8602336
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-28 23:34:14 -0600

    feat: enhance RootLayout with responsive design and image asset

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,23 @@
 import { Toaster } from "@/components/ui/sonner";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main>
+    <main className="flex min-h-screen w-full justify-between">
       {children}
-      <Toaster richColors/>
+      <div className="auth-asset">
+        <div>
+          <Image
+            src="/icons/auth-image.svg"
+            alt="Auth Asset"
+            height={500}
+            width={500}
+          />
+        </div>
+      </div>
+      <Toaster richColors />
     </main>
   );
 }
