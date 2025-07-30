20327e92c59a0e174db9665c6f81398b06a5a7fb
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-30 17:45:12 -0600

    refactor: simplify formatting in AnimatedCounter component

--- src/components/animation/AnimatedCounter.tsx
+++ src/components/animation/AnimatedCounter.tsx
@@ -4,12 +4,7 @@ import CountUp from "react-countup";
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
     <div className="w-full">
-      <CountUp 
-      end={amount} 
-      decimal={","} 
-      prefix={"$"} 
-      decimals={2} 
-      />
+      <CountUp end={amount} decimal={","} prefix={"$"} decimals={2} />
     </div>
   );
 };
