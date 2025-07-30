a11afc2e4c9adf2cd2bba995a291a21bfd635c9c
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-30 17:46:37 -0600

    refactor: improve formatting and consistency in FormFieldInput component

--- src/app/(auth)/components/FormFieldInput.tsx
+++ src/app/(auth)/components/FormFieldInput.tsx
@@ -26,17 +26,13 @@ const FormFieldInput = <T extends FieldValues>({
     <FormField
       control={control}
       name={name}
-      render={({
-        field,
-      }: {
-        field: ControllerRenderProps<T, FieldPath<T>>;
-      }) => (
+      render={({ field }: { field: ControllerRenderProps<T, FieldPath<T>> }) => (
         <FormItem className="form-item">
           <FormLabel className="form-label">{label}</FormLabel>
           <div className="flex flex-col w-full">
             <FormControl>
               <Input
-                placeholder={placeholder} 
+                placeholder={placeholder}
                 type={type}
                 {...field}
                 className="input-class"
@@ -45,10 +41,11 @@ const FormFieldInput = <T extends FieldValues>({
                 max={maxLength}
                 pattern={pattern?.source}
                 required
-                
               />
             </FormControl>
-            {description && <FormDescription className="mt-1 text-xs">{description}</FormDescription>}
+            {description && (
+              <FormDescription className="mt-1 text-xs">{description}</FormDescription>
+            )}
             <FormMessage className="form-message mt-2" />
           </div>
         </FormItem>
