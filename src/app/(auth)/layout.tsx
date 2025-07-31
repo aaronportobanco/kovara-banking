20b5112038e537ffd14ec48b7b4e49c88014ee36
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-31 12:08:35 -0600

    refactor: streamline layout component by improving formatting and type annotations

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,18 +3,13 @@ import Image from "next/image";
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: Readonly<{ children: React.ReactNode }>): React.ReactElement {
   return (
     <main className="flex min-h-screen w-full justify-between">
       {children}
       <div className="auth-asset">
         <div>
-          <Image
-            src="/icons/auth-image.svg"
-            alt="Auth Asset"
-            height={500}
-            width={500}
-          />
+          <Image src="/icons/auth-image.svg" alt="Auth Asset" height={500} width={500} />
         </div>
       </div>
       <Toaster richColors />
