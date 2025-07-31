e0851fc2fba51adc4b3d9e36a569d3a5e605306f
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-31 14:35:00 -0600

    refactor: implement user authentication check on PlaidLinkPage

--- src/app/(auth)/plaid-link/page.tsx
+++ src/app/(auth)/plaid-link/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { getLoggedInUser } from "@/services/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const PlaidLinkPage = () => {
+const PlaidLinkPage: () => Promise<JSX.Element> = async () => {
+  const user = await getLoggedInUser();
+
+  // If the user is not logged in, render the sign-up form
+  // This ensures that the plaid-link page is only accessible to unauthenticated users
+  // and prevents authenticated users from accessing it.
+  if (!user) redirect("/sign-up");
+
   return <div>PlaidLinkPage</div>;
 };
 
