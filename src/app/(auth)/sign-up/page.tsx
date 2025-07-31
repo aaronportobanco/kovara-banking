034e163ba38e745a1beea136e3f6e9d5706dfbb9
Aarón Portobanco <aaronportobanco@gmail.com>
2025-07-31 14:35:43 -0600

    refactor: explicitly define return type for SignUpPage and streamline user redirection logic

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,14 +3,11 @@ import SignUpForm from "./SignUpForm";
 import { getLoggedInUser } from "@/services/actions/user.actions";
 import { redirect } from "next/navigation";
 
-const SignUpPage = async () => {
-  //const user = await getLoggedInUser();
-  // If the user is already logged in, redirect to the Plaid link page
-  //if (user) redirect("/plaid-link");
+const SignUpPage: () => Promise<JSX.Element> = async () => {
+  const user = await getLoggedInUser();
 
-  // If the user is not logged in, render the sign-up form
-  // This ensures that the sign-up page is only accessible to unauthenticated users
-  // and prevents authenticated users from accessing it.
+  // If the user is already logged in, redirect to the Plaid link page
+  if (user) redirect("/plaid-link");
 
   return (
     <section className="flex-center size-full max-sm:px-6">
