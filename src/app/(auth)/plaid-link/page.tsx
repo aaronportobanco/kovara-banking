91e8aa0daf44d83a587adc0a9ac5132fe44132c9
Aarón Portobanco <aaronportobanco@gmail.com>
2025-08-01 11:32:17 -0600

    refactor: rename variable for clarity in PlaidLinkPage

--- src/app/(auth)/plaid-link/page.tsx
+++ src/app/(auth)/plaid-link/page.tsx
@@ -3,12 +3,12 @@ import { getLoggedInUser } from "@/services/actions/user.actions";
 import { redirect } from "next/navigation";
 
 const PlaidLinkPage: () => Promise<React.ReactElement> = async () => {
-  const user = await getLoggedInUser();
+  const userLoggedIn = await getLoggedInUser();
 
   // If the user is not logged in, render the sign-up form
   // This ensures that the plaid-link page is only accessible to unauthenticated users
   // and prevents authenticated users from accessing it.
-  if (!user) redirect("/sign-up");
+  if (!userLoggedIn) redirect("/sign-up");
 
   return <div>PlaidLinkPage</div>;
 };
