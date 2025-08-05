557135734831c5ae17d10d35669a771657d7cd2b
Aarón Portobanco <aaronportobanco@gmail.com>
2025-08-05 17:37:08 -0600

    fix: implement user redirection on successful login in SignInPage component

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import SignInForm from "./SignInForm";
+import { getLoggedInUser } from "@/services/actions/user.actions";
+import { redirect } from "next/navigation";
 
 const SignInPage: () => Promise<React.ReactElement> = async () => {
+  const user = await getLoggedInUser();
+
+  if (user) {
+    redirect("/");
+  }
+
   return (
     <section className="flex-center size-full max-sm:px-6">
       <SignInForm />
